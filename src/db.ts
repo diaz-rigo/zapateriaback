88dbde57162ad1501668066af564d8ac933d5860
meandro02 <rd209422@gmail.com>
2024-04-21 11:05:33 -0600

    oo1

--- src/db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "npm:mongoose";
-
-try {
-  const conn = await mongoose.connect("mongodb+srv://rd209422:Cf3fSt1hdv9rf8Ud@cluster0.reakihy.mongodb.net/huejutla");
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
-} catch (error) {
-  console.error(error);
-}
